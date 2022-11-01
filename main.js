7e96a695bc67ba28035c2362777e62404273bf54
Bamtexh <103510550+Bamtexh@users.noreply.github.com>
2022-11-01 11:51:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+// SLIDER
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    dots:false,
+    nav:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+// REVIEWS SLIDER
+$('#reviews-carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    dots:true,
+    nav:false,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
